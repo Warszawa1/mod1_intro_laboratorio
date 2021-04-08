120c150ae030865f99ade5dd5d1d13d9c000c9b2
Warszawa1 <48474962+Warszawa1@users.noreply.github.com>
2021-04-08 23:00:39 +0200

    Update demo.js

--- js/demo.js
+++ js/demo.js
@@ -1,4 +1,3 @@
 document.getElementById("name").value = "Mi nombre";
 document.getElementById("lastName").value = "Mi apellido";
-// diff
-document.getElementById("avatar").src = "img/avatar.png";
+
